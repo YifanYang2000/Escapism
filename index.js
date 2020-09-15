e5cb487d1bf2407ca177841366e388b1774488dd
YifanYang2000 <yifan.yang@hotmail.ca>
2020-09-15 00:05:53 -0400

    connect to mongoDB and moved private info on dev.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Yifan:3015edmond@mern.h9lyn.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const config = require('./config/key')
+
+const {User} = require('./models/user')
+
+mongoose.connect(config.mongoURI, 
     {useNewUrlParser: true}).then(() =>console.log('DB connected'))
                             .catch(err => console.error(err));
 
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-});
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userData) => {
+        if(err) return res.json ({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
 
 app.listen(5000);
