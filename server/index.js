ac721f0de57fb477dd540db0ee078edd583c75a8
YifanYang2000 <yifan.yang@hotmail.ca>
2020-09-16 01:14:03 -0400

    logout done

--- server/index.js
+++ server/index.js
@@ -75,12 +75,17 @@ app.post('/api/user/login', (req, res) => {
                     loginSuccess: true
                 });
         });
-    })
-
-    
-
+    });
 });
 
+app.get("/api/user/logout", auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id}, {token: ""}, (err, doc) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).send({
+           success: true 
+        })
+    });
+});
 
 
 app.listen(5000);
